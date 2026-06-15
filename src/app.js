2b38661178e390b0b414d03dbe8bb86bd34c2c1f
Mitul Ghumaliya <pmitulghumaliya53@gmail.com>
2026-06-15 21:26:37 +0530

    config cors for application.

--- src/app.js
+++ src/app.js
@@ -8,12 +8,16 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
-app.use(cors());
+app.use(
+    cors({
+        origin: "*",
+    })
+);
 
 app.use('/api', appRoute);
 
-app.get('/', async (req,res) => {
-    return res.send("Hello Wordls")
+app.get('/', async (req, res) => {
+    return res.send("hello world !!!")
 });
 
 export { app };
